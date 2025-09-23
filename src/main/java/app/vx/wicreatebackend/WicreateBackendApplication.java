7a104eadcf7b4d331c2e05c17a80a107e5fdea39
Vortex-java <daniilpasenko760@gmail.com>
2025-09-23 23:03:38 +0300

    начало создания бэкенда

---
+++ src/main/java/app/vx/wicreatebackend/WicreateBackendApplication.java
@@ -0,0 +1,13 @@
+package app.vx.wicreatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WicreateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WicreateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/vx/wicreatebackend/WicreateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.vx.wicreatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WicreateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
